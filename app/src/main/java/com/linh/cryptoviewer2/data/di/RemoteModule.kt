72fb50e6ae3934c8372c686a63b5eb2e1fcc4cb5
Linh Van Nguyen <linhvnguyen9@gmail.com>
2022-07-27 17:42:39 +0700

    Add get coin service

---
+++ app/src/main/java/com/linh/cryptoviewer2/data/di/RemoteModule.kt
@@ -0,0 +1,40 @@
+package com.linh.cryptoviewer2.data.di
+
+import com.linh.cryptoviewer2.data.remote.service.CoinService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteModule {
+    @Singleton
+    @Provides
+    fun provideOkhttp() = OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.HEADERS
+            }
+        )
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://api.coingecko.com/api/v3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideCoinService(retrofit: Retrofit): CoinService =
+        retrofit.create(CoinService::class.java)
+}
---
+++ app/src/main/java/com/linh/cryptoviewer2/data/remote/model/GetCoinResponse.kt
@@ -0,0 +1,36 @@
+package com.linh.cryptoviewer2.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class GetCoinResponse(
+    val id: String,
+    val symbol: String,
+    val name: String,
+    val image: Image,
+    @SerializedName("sentiment_votes_up_percentage")
+    val sentimentsVoteUpPercentage: Float,
+    @SerializedName("sentiment_votes_down_percentage")
+    val sentimentsVoteDownPercentage: Float,
+    @SerializedName("market_cap_rank")
+    val marketCapRank: Int,
+    @SerializedName("coingecko_rank")
+    val coinGeckoRank: Int,
+    @SerializedName("market_data")
+    val marketData: MarketData
+) {
+    data class Image(
+        @SerializedName("thumb") val thumbUrl: String,
+        @SerializedName("small") val smallUrl: String,
+        @SerializedName("large") val largeUrl: String
+    )
+
+    data class MarketData(
+        @SerializedName("current_price") val currentPrice: CurrentPrice
+    ) {
+        data class CurrentPrice(
+            val usd: Double,
+            val vnd: Double,
+            val btc: Double
+        )
+    }
+}
