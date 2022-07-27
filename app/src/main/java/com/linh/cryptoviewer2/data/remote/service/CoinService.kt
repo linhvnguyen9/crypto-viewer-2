f8de71fd30bb228e485c7c95f907273c0cbac4da
Linh Van Nguyen <linhvnguyen9@gmail.com>
2022-07-27 18:14:51 +0700

    Add CoinService integration test

--- app/src/main/java/com/linh/cryptoviewer2/data/remote/service/CoinService.kt
+++ app/src/main/java/com/linh/cryptoviewer2/data/remote/service/CoinService.kt
@@ -1,9 +1,10 @@
 package com.linh.cryptoviewer2.data.remote.service
 
+import com.linh.cryptoviewer2.data.remote.model.GetCoinResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface CoinService {
     @GET("coin/{id}")
-    suspend fun getCoin(@Path("id") id: String)
+    suspend fun getCoin(@Path("id") id: String): GetCoinResponse
 }
