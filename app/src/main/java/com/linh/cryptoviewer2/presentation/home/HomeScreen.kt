ce1921a6fdfada2a9a4490f4a69f9f675202686f
Linh Van Nguyen <linhvnguyen9@gmail.com>
2022-08-30 11:29:39 +0700

    Add search immediately option for home screen

--- app/src/main/java/com/linh/cryptoviewer2/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/linh/cryptoviewer2/presentation/home/HomeScreen.kt
@@ -3,6 +3,8 @@ package com.linh.cryptoviewer2.presentation.home
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
@@ -10,6 +12,7 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.linh.cryptoviewer2.presentation.home.model.HomeScreenUiState
@@ -57,10 +60,10 @@ private fun LoadingState() {
 }
 
 @Composable
-private fun SearchHeader(query: String?, onQueryChange: (String) -> Unit) {
+private fun SearchHeader(query: String?, onQueryChange: (query: String, shouldSearchImmediately: Boolean) -> Unit) {
     TextField(
         value = query.orEmpty(),
-        onValueChange = onQueryChange,
+        onValueChange = { onQueryChange(it, false) },
         modifier = Modifier.fillMaxWidth(),
         maxLines = 1,
         leadingIcon = {
@@ -69,18 +72,20 @@ private fun SearchHeader(query: String?, onQueryChange: (String) -> Unit) {
         trailingIcon = {
             if (!query.isNullOrBlank()) {
                 IconButton(
-                    onClick = { onQueryChange("") },
+                    onClick = { onQueryChange("", false) },
                     content = {
                         Icon(Icons.Filled.Clear, null)
                     }
                 )
             }
-        }
+        },
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = { onQueryChange(query.orEmpty(), true)} )
     )
 }
 
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen(uiState = HomeScreenUiState.Initial {})
+    HomeScreen(uiState = HomeScreenUiState.Initial(onQueryTextChange = { query: String, shouldSearchImmediately: Boolean ->  }))
 }
